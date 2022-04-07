23f40dc7e204b5b80e5be3638aee6cea115fd6c0
Brrright <mingliang0524@gmail.com>
2022-04-07 23:57:05 +0800

    uh i need submit this, just leave it first

---
+++ common/header_admin.php
@@ -0,0 +1,51 @@
+<nav class="navbar fixed-top navbar-expand-lg">
+    <div class="container-fluid">
+        <!-- Some link need to use if else statement to do, will add on soon (to check session, knowing if user login onot) -->
+        <a class="navbar-brand logo-style ms-2" href="#">
+            <img src="img/logo_small_no_text.png" alt="Examomo logo" width="40px" height="40px" >
+            Examomo
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 me-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Manage Module</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Manage Class</a>
+                </li>
+                
+
+                <!-- Drop down Nav Item -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Manage Account
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Student</a></li>
+                        <li><a class="dropdown-item" href="#">Lecturer</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Admin</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <li class="nav-item">
+                <a class="nav-link position-relative" href="#">
+                    Feedback 
+                    <span id="" class="position-absolute top-0 start-100 translate-middle badge bg-warning rounded-pill text-dark">1</span>
+                </a>
+            </li>
+
+            <!-- Search bar -->
+            <!-- <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+            </form> -->
+        </div>
+    </div>
+</nav>
