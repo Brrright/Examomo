a3d619fc138592ad500a474f6fa230bd5686e188
Brrright <mingliang0524@gmail.com>
2022-04-07 23:56:22 +0800

    neccessary for upcoming changes (header)

--- common/HeadImportInfo.php
+++ common/HeadImportInfo.php
@@ -11,9 +11,10 @@ crossorigin="anonymous"/>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
 integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" 
 crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
 <!-- logo icon -->
-<link rel="icon" href="image/ExamomoLogo_transparent_noSlogan.png" type="image/ico">
+<link rel="icon" href="img/logo_small_no_text.png" type="image/ico">
 
 <!-- sweet alert pop up -->
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
---
+++ guest_home_page.php
@@ -0,0 +1,19 @@
+<?php
+    require "common/conn.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php 
+        require "common/HeadImportInfo.php";
+    ?>
+    <link rel="stylesheet" href="css/commonCSS.css">
+    <title>Examomo | Guest Home Page</title>
+</head>
+<body>
+    <?php 
+        // require "common/header_guest.php";
+        require "common/header_admin.php";
+    ?>
+</body>
+</html>
