099b28672caea3a75103f7748250cf7a32f98782
Brrright <mingliang0524@gmail.com>
2022-05-23 23:07:58 +0800

    tzehoe ui

--- lecturer_home_page.php
+++ lecturer_home_page.php
@@ -20,6 +20,7 @@
         require "common/HeadImportInfo.php" 
     ?>
 
+    <link rel="stylesheet" href="css/weestyle.css">
     <link rel="stylesheet" href="css/bryanCSS.css">
     <link rel="stylesheet" href="css/commonCSS.css"> 
 
@@ -52,8 +53,10 @@
   </div>
 
   <!-- Main function carousel -->
+
   <section class="carousel-section">
-    <div class="container carousel">
+  <div class="admincontainer my-0 p-6 shadow p-3 mb-5">
+    <div class="container carousel p-0">
       <div id="carouselIndicators" class="carousel slide carousel-fade" data-bs-ride="carousel">
         <div class="carousel-indicators">
           <button type="button" data-bs-target="#carouselIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -67,10 +70,10 @@
             <div class="overlay-image" style="background-image:url(img/lecturer/exam.jpg);"></div>
             <div class="container-carousel">
             <h2>Examinations</h2>
-            <p>Create and Organize your Examinations in Examomo</p>
-            <a href ="lecturer_exam_page.php" class="btn btn-lg btn-primary">
-              Manage Exams
-            </a>
+            <p style="color:white;">Create and Organize your Examinations in Examomo</p>
+            <div>
+              <a href ="lecturer_exam_page.php" class="stubtn">Manage Exams</a>
+            </div>
             </div>
           </div>
   
@@ -79,8 +82,8 @@
             <div class="overlay-image" style="background-image:url(img/lecturer/exampaper.jpg);"></div>
             <div class="container-carousel">
             <h2>Exam Papers</h2>
-            <p>Create and Manage your Exam Papers in Multiple Choice or Structured Format</p>
-            <a href ="lecturer_exampaper_page.php" class="btn btn-lg btn-primary">
+            <p style="color:white;">Create and Manage your Exam Papers in Multiple Choice or Structured Format</p>
+            <a href ="lecturer_exampaper_page.php" class="stubtn">
               Manage Exam papers
             </a>
             </div>
@@ -91,8 +94,8 @@
             <div class="overlay-image" style="background-image:url(img/lecturer/completedexam.jpg);"></div>
             <div class="container-carousel">
               <h2>Completed Exams</h2>
-              <p>Manage your Completed Exams and Review Students' Exam Papers here</p>
-              <a href ="#" class="btn btn-lg btn-primary">
+              <p style="color:white;">Manage your Completed Exams and Review Students' Exam Papers here</p>
+              <a href ="#" class="stubtn">
                 Check Completed Exams
               </a>
               </div>
@@ -109,7 +112,7 @@
           <span class="visually-hidden">Next</span>
         </button>
       </div>
-      <br>
+    </div>
     </div>
     </section>
     <br>
@@ -118,156 +121,150 @@
     <section>
     <center><h2 style="font-family: 'Caveat'; font-weight: bold; color: #2B5EA4;">Your Most Recent</h2></center> 
     <br>
-    <div class= "d-flex flex-wrap justify-content-around" style="min-height: 20rem;"> 
-      <!-- Exam card -->
-      <div class= "col-sm-6" style= "width: 25rem; margin-bottom: 40px;">
-        <div class="card-lecturerhp">
-          <div class="card-body-lecturerhp">
-            <h3 class="card-title-lecturerhp"><i class="bi bi-calendar3-event"></i> Published Exam</h3>
-            <p class="card-text-lecturerhp" style="color: #2B5EA4;">
-              
-              <!-- retrieve exam details based on lecturer id -->
-              <?php
-                $sql = "SELECT ExamName, ExamStartDateTime, ExamEndDateTime, ExamDescription FROM exam WHERE LecturerID = '".$_SESSION["userID"]."' AND isPublished LIKE 1 ORDER BY ExamID DESC LIMIT 1";   
-                $result = mysqli_query($con, $sql);
-  
-                while ($row = mysqli_fetch_array($result)) {
-  
-                  echo "<br>"."Exam Name: ";
-                  echo $row['ExamName']."<br>"."<br>";
-  
-                  echo "Start Date & Time: ";
-                  echo $row['ExamStartDateTime']."<br>"."<br>";
-  
-                  echo "End Date & Time: ";
-                  echo $row['ExamEndDateTime']."<br>"."<br>";
-  
-                  echo "Description: ";
-                  echo $row['ExamDescription']."<br>"."<br>";
-                  
-              }
-                
-              ?>
-            </p>
-            <div style="text-align: center;">
-              <a href="lecturer_exam_page.php" class="btn btn-primary">View All Exams</a>
+    <div class="container">
+      <div class="row justify-content-evenly">
+            <div class="col-sm col-xs-12">
+              <!-- Exam card -->
+              <div class= "card shadow p-3 mb-5" style="width: 18rem;">
+                  <div class="card-body">
+                    <h4 class="card-title-lecturerhp"><i class="bi bi-calendar3-event"></i> Published Exam</h4>
+                    <p class="card-text" style="color: #2B5EA4;">
+                      
+                      <!-- retrieve exam details based on lecturer id -->
+                      <?php
+                        $sql = "SELECT ExamName, ExamStartDateTime, ExamEndDateTime, ExamDescription FROM exam WHERE LecturerID = '".$_SESSION["userID"]."' AND isPublished LIKE 1 ORDER BY ExamID DESC LIMIT 1";   
+                        $result = mysqli_query($con, $sql);
+          
+                        while ($row = mysqli_fetch_array($result)) {
+          
+                          echo "<br>"."Exam Name: <br>";
+                          echo $row['ExamName']."<br>"."<br>";
+          
+                          echo "Start Date & Time: <br>";
+                          echo $row['ExamStartDateTime']."<br>"."<br>";
+          
+                          echo "End Date & Time: <br>";
+                          echo $row['ExamEndDateTime']."<br>"."<br>";
+          
+                          echo "Description: <br>";
+                          echo $row['ExamDescription']."<br>"."<br>";
+                          
+                      }
+                        
+                      ?>
+                    </p>
+                    <br>
+                      <a href="lecturer_exam_page.php" style="font-size: 14px;" class="stubtn">View All Exams</a>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </div>
   
       <!-- Drafted exam card -->
-      <div class= "col-sm-6" style= "width: 25rem; margin-bottom: 40px;">
-        <div class="card-lecturerhp">
-          <div class="card-body-lecturerhp">
-            <h3 class="card-title-lecturerhp"><i class="bi bi-file-earmark"></i> Drafted Exam</h3>
-            <p class="card-text-lecturerhp" style="color: #2B5EA4;">
-            <?php
-                $sql = "SELECT ExamName, ExamStartDateTime, ExamEndDateTime, ExamDescription FROM exam WHERE LecturerID = '".$_SESSION["userID"]."' AND isPublished LIKE 0 ORDER BY ExamID DESC LIMIT 1";   
-                $result = mysqli_query($con, $sql);
-  
-                while ($row = mysqli_fetch_array($result)) {
-  
-                  echo "<br>"."Exam Name: ";
-                  echo $row['ExamName']."<br>"."<br>";
-  
-                  echo "Start Date & Time: ";
-                  echo $row['ExamStartDateTime']."<br>"."<br>";
+            <div class="col-sm col-xs-12">
+              <div class= "card shadow p-3 mb-5" style="width: 18rem;">
+                <div class="card-body">
+                  <h4 class="card-title-lecturerhp"><i class="bi bi-file-earmark"></i> Drafted Exam</h4>
+                  <p class="card-text" style="color: #2B5EA4;">
+                  <?php
+                      $sql = "SELECT ExamName, ExamStartDateTime, ExamEndDateTime, ExamDescription FROM exam WHERE LecturerID = '".$_SESSION["userID"]."' AND isPublished LIKE 0 ORDER BY ExamID DESC LIMIT 1";   
+                      $result = mysqli_query($con, $sql);
+        
+                      while ($row = mysqli_fetch_array($result)) {
+        
+                        echo "<br>"."Exam Name: <br>";
+                        echo $row['ExamName']."<br>"."<br>";
+        
+                        echo "Start Date & Time: <br>";
+                        echo $row['ExamStartDateTime']."<br>"."<br>";
 
-                  echo "End Date & Time: ";
-                  echo $row['ExamEndDateTime']."<br>"."<br>";
-  
-                  echo "Description: ";
-                  echo $row['ExamDescription']."<br>"."<br>";
-                  
-              }
-                
-              ?>
-            </p>
-            <div style="text-align: center;">
-              <a href="lecturer_exam_page.php" class="btn btn-primary cardbtn" >View All Exams</a>
+                        echo "End Date & Time: <br>";
+                        echo $row['ExamEndDateTime']."<br>"."<br>";
+        
+                        echo "Description: <br>";
+                        echo $row['ExamDescription']."<br>"."<br>";
+                        
+                    }
+                      
+                    ?>
+                  </p>
+                  <br>
+                    <a href="lecturer_exam_page.php" style="font-size: 14px;" class="stubtn" >View All Exams</a>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </div>
 
       <!-- Completed exam card -->
-      <div class= "col-sm-6" style= "width: 25rem; margin-bottom: 40px;">
-        <div class="card-lecturerhp">
-          <div class="card-body-lecturerhp">
-            <h3 class="card-title-lecturerhp"><i class="bi bi-check2-square"></i> Completed Exam</h3>
-            <p class="card-text-lecturerhp" style="color: #2B5EA4;">
-            <?php
-                // get current datetime
-                date_default_timezone_set('Asia/Kuala_Lumpur');
-                $date_clicked = date('Y-m-d H:i:s');
-                $sql = "SELECT * FROM exam WHERE ExamEndDateTime <= '$date_clicked' AND LecturerID = '".$_SESSION["userID"]."' AND isPublished LIKE 1 ORDER BY ExamID DESC LIMIT 1";  
-                $result = mysqli_query($con, $sql);
-  
-                while ($row = mysqli_fetch_array($result)) {
+            <div class="col-sm col-xs-12">
+              <div class= "card shadow p-3 mb-5" style="width: 18rem;">
+                <div class="card-body">
+                  <h4 class="card-title-lecturerhp"><i class="bi bi-check2-square"></i> Completed Exam</h4>
+                  <p class="card-text" style="color: #2B5EA4;">
+                  <?php
+                      // get current datetime
+                      date_default_timezone_set('Asia/Kuala_Lumpur');
+                      $date_clicked = date('Y-m-d H:i:s');
+                      $sql = "SELECT * FROM exam WHERE ExamEndDateTime <= '$date_clicked' AND LecturerID = '".$_SESSION["userID"]."' AND isPublished LIKE 1 ORDER BY ExamID DESC LIMIT 1";  
+                      $result = mysqli_query($con, $sql);
+        
+                      while ($row = mysqli_fetch_array($result)) {
 
-                  echo "<br>"."Exam Name: ";
-                  echo $row['ExamName']."<br>"."<br>";
-  
-                  echo "Exam started at: ";
-                  echo $row['ExamStartDateTime']."<br>"."<br>";
-  
-                  echo "Exam ended at: ";
-                  echo $row['ExamEndDateTime']."<br>"."<br>";
-                  
-                }
-                
-                ?>
-            </p>
-            <div style="text-align: center;">
-              <a href="#" class="btn btn-primary" id ="cardbutton">View All Completed Exams</a>
+                        echo "<br>"."Exam Name: <br>";
+                        echo $row['ExamName']."<br>"."<br>";
+        
+                        echo "Exam started at: <br>";
+                        echo $row['ExamStartDateTime']."<br>"."<br>";
+        
+                        echo "Exam ended at: <br>";
+                        echo $row['ExamEndDateTime']."<br>"."<br>";
+                        
+                      }
+                      
+                      ?>
+                  </p>
+                  <br>
+                    <a href="lecturer_completed_exam_list.php" class="stubtn" style="font-size: 14px;" id ="cardbutton">View All Completed Exams</a>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </div>
+
 
             <!-- Exam paper card -->
-            <div class= "col-sm-6" style= "width: 25rem; margin-bottom: 40px;">
-        <div class="card-lecturerhp">
-          <div class="card-body-lecturerhp">
-            <h3 class="card-title-lecturerhp"><i class="bi bi-list-ul"></i> Exam Paper</h3>
-            <p class="card-text-lecturerhp" style="color: #2B5EA4;">
-            <?php
-                $sql = "SELECT exam_paper.PaperName, exam_paper.DateCreated, module.ModuleName, exam_paper.PaperType FROM exam_paper INNER JOIN module ON exam_paper.ModuleID = module.ModuleID WHERE exam_paper.LecturerID = '".$_SESSION["userID"]."' ORDER BY exam_paper.DateCreated DESC LIMIT 1";  
-                $result = mysqli_query($con, $sql);
-  
-                while ($row = mysqli_fetch_array($result)) {
+          
+              <div class="col-sm col-xs-12">
+                <div class= "card shadow p-3 mb-5" style="width: 18rem;">
+                  <div class="card-body">
+                    <h4 class="card-title-lecturerhp"><i class="bi bi-list-ul"></i> Exam Paper</h4>
+                    <p class="card-text" style="color: #2B5EA4;">
+                    <?php
+                        $sql = "SELECT exam_paper.PaperName, exam_paper.DateCreated, module.ModuleName, exam_paper.PaperType FROM exam_paper INNER JOIN module ON exam_paper.ModuleID = module.ModuleID WHERE exam_paper.LecturerID = '".$_SESSION["userID"]."' ORDER BY exam_paper.DateCreated DESC LIMIT 1";  
+                        $result = mysqli_query($con, $sql);
+          
+                        while ($row = mysqli_fetch_array($result)) {
 
-                  echo "<br>"."Paper Name: ";
-                  echo $row['PaperName']."<br>"."<br>";
-  
-                  echo "Creation Date: ";
-                  echo $row['DateCreated']."<br>"."<br>";
-  
-                  echo "Module Name: ";
-                  echo $row['ModuleName']."<br>"."<br>";
-                  
-                  echo "Type: ";
-                  echo $row['PaperType']."<br>"."<br>";
-                  
-                }
-                
-                ?>
-            </p>
-            <div style="text-align: center;">
-              <a href="lecturer_exampaper_page.php" class="btn btn-primary" id ="cardbutton">View All Exam Papers</a>
+                          echo "<br>"."Paper Name: <br>";
+                          echo $row['PaperName']."<br>"."<br>";
+          
+                          echo "Creation Date: <br>";
+                          echo $row['DateCreated']."<br>"."<br>";
+          
+                          echo "Module Name: <br>";
+                          echo $row['ModuleName']."<br>"."<br>";
+                          
+                          echo "Type: <br>";
+                          echo $row['PaperType']."<br>"."<br>";
+                          
+                        }
+                        
+                        ?>
+                    </p>
+                    <br>
+                      <a href="lecturer_exampaper_page.php" class="stubtn" style="font-size: 14px;" id ="cardbutton">View All Exam Papers</a>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-
-    </div>
   </section>
-  <br><br>
-
-    
-
-
 <!-- footer -->
 <?php include "./common/footer_lecturer.php" ?>
 </body>
